cac38070f3d1d12390bda92891fb59c38cb11968
Hokori23 <524159845@qq.com>
2020-04-10 22:21:39 +0800

    Beta1.0 布局 by Hokori

---
+++ script/main/search/input.js
@@ -0,0 +1,18 @@
+/* * * * * * * * * * * * * * * * * * *
+ *
+ *				DOM
+ *
+ * * * * * * * * * * * * * * * * * * */
+const inputCheck = () => {
+	const input = $('.left_header_search_input');
+	let timer = null;
+	input.keyup(() => {
+		// if(timer!==null){
+		// 	clearTimeout(timer)
+		// }
+		// timer = setTimeout(()=>{
+			
+		// },500)
+	})
+}
+inputCheck()
